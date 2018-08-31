57b1a0a8e5630d55d4f719fd46842c40c120e2c4
Benjie Gillam <benjie@jemjie.com>
2018-08-31 15:52:01 +0100

    Yarn and NPM mode

--- scripts/make-cache.sh
+++ scripts/make-cache.sh
@@ -8,8 +8,18 @@ if ! [ -x "$(command -v zip)" ]; then
     apt-get update && apt-get install zip -y
 fi
 
-npm i -g yarn@latest
-yarn install --force
+if [ -f package-lock.json ]; then
+  # npm mode
+  npm i -g npm@latest
+  npm i
 
-sleep 3
-yarn bundle
+  sleep 3
+  npm run bundle
+else
+  # yarn mode
+  npm i -g yarn@latest
+  yarn install --force
+
+  sleep 3
+  yarn bundle
+fi
