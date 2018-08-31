ddc20699ce223a81ad1c5b6f2d770db2df259874
Benjie Gillam <benjie@jemjie.com>
2018-08-31 16:15:52 +0100

    Enable SAM to connect to the database

--- make-template-yml.sh
+++ make-template-yml.sh
@@ -12,11 +12,12 @@ Resources:
     Properties:
       Environment:
         Variables:
-          DATABASE_URL: "postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@pg10/${POSTGRES_DB}"
+          DATABASE_URL: "postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@host.docker.internal:54321/${POSTGRES_DB}"
           DATABASE_SCHEMAS: "${DATABASE_SCHEMAS}"
           JWT_SECRET: "${JWT_SECRET}"
       Runtime: nodejs8.10
       Handler: index.handler
+      Timeout: 30
       CodeUri: ./src/lambda.zip
       Events:
         Api:
