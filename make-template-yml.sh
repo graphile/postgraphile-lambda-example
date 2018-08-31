ffe84731959c63b54c30130b4176c51330fd0d17
Benjie Gillam <benjie@jemjie.com>
2018-08-31 15:49:39 +0100

    Template constructor

--- make-template-yml.sh
+++ make-template-yml.sh
@@ -1,15 +1,20 @@
+#!/bin/bash
+if [ -x "./.env" ]; then
+  . ./.env;
+fi;
+cat > template.yml <<HERE
 AWSTemplateFormatVersion : '2010-09-09'
 Transform: AWS::Serverless-2016-10-31
-Description: Postgraphile as a Lambda Function
+Description: PostGraphile as a Lambda Function
 Resources:
   graphql:
     Type: AWS::Serverless::Function
     Properties:
       Environment:
         Variables:
-          DATABASE_URL: postgres://gqluser:gqlGQL000@postgres:5432/postgres
-          DATABASE_SCHEMAS: forum_example,forum_example_private
-          JWT_SECRET: "2c045e2eb2ec46488530"
+          DATABASE_URL: "postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@pg10/${POSTGRES_DB}"
+          DATABASE_SCHEMAS: "${DATABASE_SCHEMAS}"
+          JWT_SECRET: "${JWT_SECRET}"
       Runtime: nodejs8.10
       Handler: index.handler
       CodeUri: ./src/lambda.zip
@@ -19,3 +24,4 @@ Resources:
           Properties:
             Path: /graphql
             Method: post
+HERE
