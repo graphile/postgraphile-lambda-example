cf5df3364b46bae92946a3496e161e1f73eb3bb1
Benjie Gillam <benjie@jemjie.com>
2018-08-31 16:44:57 +0100

    Test with timings

--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
+const startTimestamp = process.hrtime();
+const timestamps = [];
+timestamps.push(['start', process.hrtime(startTimestamp)]);
 const express = require('express');
+timestamps.push(['required express', process.hrtime(startTimestamp)]);
 const awsServerlessExpress = require("aws-serverless-express");
+timestamps.push(['required aws-serverless-express', process.hrtime(startTimestamp)]);
 const { postgraphile } = require("postgraphile");
+timestamps.push(['required postgraphile', process.hrtime(startTimestamp)]);
 const { options, cachePath } = require("./postgraphileOptions");
+timestamps.push(['required postgraphileOptions', process.hrtime(startTimestamp)]);
 const bodyParser = require('body-parser')
+timestamps.push(['required body-parser', process.hrtime(startTimestamp)]);
 
 const schemas = process.env.DATABASE_SCHEMAS ? process.env.DATABASE_SCHEMAS.split(",") : ['app_public'];
-console.log('-=-=-=-=-=-=-=-=-=')
 const app = express();
+timestamps.push(['express created', process.hrtime(startTimestamp)]);
 app.use(postgraphile(process.env.DATABASE_URL, schemas, {
   ...options,
   readCache: cachePath,
 }));
+timestamps.push(['postgraphile mounted', process.hrtime(startTimestamp)]);
 
 // @see https://github.com/Yelp/yelp-fusion/issues/278
 app.use(/\/((?!graphql).)*/, bodyParser.urlencoded({ extended: true }));
 app.use(/\/((?!graphql).)*/, bodyParser.json());
+timestamps.push(['middlewares mounted', process.hrtime(startTimestamp)]);
 
 const server = awsServerlessExpress.createServer(app);
 exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context)
-
-
-// 
+timestamps.push(['finished', process.hrtime(startTimestamp)]);
+for (const [msg, interval] of timestamps) {
+  const formattedInterval = (interval[0] * 1000 + interval[1] * 1e-6).toFixed(2);
+  console.log(`${formattedInterval}ms: ${msg}`);
+}
---
+++ test.sh
@@ -0,0 +1,35 @@
+set -e
+
+echo "Making template.yml"
+./make-template-yml.sh
+
+echo "Spinning up SAM... (waiting a few seconds)"
+trap 'kill $(jobs -p)' EXIT
+trap 'kill $(jobs -p)' SIGINT
+sam local start-api &
+SAM=$!
+sleep 5
+
+echo "Attempting a warm-up request..."
+curl -i -XPOST \
+  -H 'Accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d @query.graphql \
+  http://127.0.0.1:3000/graphql
+
+echo "Running a few more requests"
+
+for I in `seq 1 10`; do
+  time curl -i -XPOST \
+    -H 'Accept: application/json' \
+    -H 'Content-Type: application/json' \
+    -d @query.graphql \
+    http://127.0.0.1:3000/graphql
+done
+
+echo "Shutting SAM back down..."
+kill $SAM
+trap - EXIT
+trap - SIGINT
+wait
+echo "Done"
