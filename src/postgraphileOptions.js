ae29608bd6b949727bcedb1edf9c2cd3d6ddfbb9
Benjie Gillam <benjie@jemjie.com>
2018-09-06 16:31:05 +0100

    Disable jwtPgTypeIdentifier for test

--- src/postgraphileOptions.js
+++ src/postgraphileOptions.js
@@ -2,5 +2,5 @@ exports.options = {
   dynamicJson: true,
   graphiql: false,
   jwtSecret: process.env.JWT_SECRET || String(Math.random()),
-  jwtPgTypeIdentifier: "forum_example.jwt_token",
+  //jwtPgTypeIdentifier: "forum_example.jwt_token",
 };
