94937838d7eec13a26c7145897828b4cd006a3f9
Benjie Gillam <benjie@jemjie.com>
2018-11-02 16:10:00 +0000

    Add note

--- src/postgraphileOptions.js
+++ src/postgraphileOptions.js
@@ -1,6 +1,12 @@
 exports.options = {
   dynamicJson: true,
   graphiql: false,
+  /* If you enable GraphiQL, you must use `absoluteRoutes` and give the routes to the endpoints.:
+
+    absoluteRoutes: true,
+    graphqlRoute: '/default/graphql',
+    graphiqlRoute: '/default/graphiql',
+  */
   cors: true,
   jwtSecret: process.env.JWT_SECRET || String(Math.random()),
 };
