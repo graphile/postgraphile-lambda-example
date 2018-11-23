10f6422de66bb20ba4bdb8f1f37815b920dd38d1
Benjie Gillam <benjie@jemjie.com>
2018-11-23 16:07:43 +0000

    Make JWT clearer

--- src/postgraphileOptions.js
+++ src/postgraphileOptions.js
@@ -1,9 +1,13 @@
 exports.options = {
   dynamicJson: true,
   cors: true,
-  jwtSecret: process.env.JWT_SECRET || String(Math.random()),
   graphiql: false,
   externalUrlBase: '/default',
+
+  // If consuming JWT:
+  jwtSecret: process.env.JWT_SECRET || String(Math.random()),
+  // If generating JWT:
+  jwtPgTypeIdentifier: 'forum_example.jwt_token',
 
   /* If you want to enable GraphiQL, you must use `externalUrlBase` so PostGraphile
    * knows where to tell the browser to find the assets.  Doing this is
