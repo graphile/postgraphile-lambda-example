63bd09e417eb7f45becdff8abe981d6f95438996
Benjie Gillam <benjie@jemjie.com>
2018-11-23 15:51:08 +0000

    Update to RC2

--- src/postgraphileOptions.js
+++ src/postgraphileOptions.js
@@ -1,13 +1,17 @@
 exports.options = {
   dynamicJson: true,
   graphiql: false,
-  /* If you want to enable GraphiQL, you must use `absoluteRoutes` and give the routes
-   * to the endpoints. Doing this is strongly discouraged, you should use an external
-   * GraphQL client instead.
+  externalUrlBase: '/default',
 
-    absoluteRoutes: true,
-    graphqlRoute: '/default',
-    graphiqlRoute: '/default/graphiql',
+
+  /* If you want to enable GraphiQL, you must use `externalUrlBase` so PostGraphile
+   * knows where to tell the browser to find the assets.  Doing this is
+   * strongly discouraged, you should use an external GraphQL client instead.
+
+    externalUrlBase: '/default',
+    graphiql: true,
+    graphqlRoute: '/',
+    graphiqlRoute: '/graphiql',
   */
   cors: true,
   jwtSecret: process.env.JWT_SECRET || String(Math.random()),
