87cbc7b59fa178fb4e96442cc6cf53f64468b4fb
Benjie Gillam <benjie@jemjie.com>
2019-01-16 09:52:03 +0000

    Add notes

--- src/index.js
+++ src/index.js
@@ -14,7 +14,11 @@ const app = combineMiddlewares([
    *
    * This is typically useful for augmenting the request before it goes to PostGraphile.
    */
+  
+  // CORS middleware to permit cross-site API requests. Configure to taste
   cors(),
+  
+  // Determines the effective URL we are at if `absoluteRoutes` is set
   (req, res, next) => {
     if (options.absoluteRoutes) {
       try {
